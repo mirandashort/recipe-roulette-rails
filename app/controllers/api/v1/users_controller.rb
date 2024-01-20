2a26639ff8f556c5726b98189e03bcb8a45959ba
Miranda Humphries <mird2794@gmail.com>
2024-01-20 11:58:24 -0500

    no method for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create_shopping_list
-    user = User.find(params[:user_id])
-
-    @shopping_list = user.create_shopping_list!
+    @user ||= User.find(params[:user_id])
+    @shopping_list = @user.create_shopping_list!
 
     render json: @shopping_list
   end
 
   def check_item
-    user = User.find(params[:user_id])
     item_shopping_list_id = ItemShoppingList.find(item_params[:item_shopping_list_id])
     item.update!(checked: true)
 
     render json: { item: item_shopping_list_id, checked: true }
-  end
-
-  def user
-    @user ||= User.find(params[:user_id])
   end
 
   def item_params
