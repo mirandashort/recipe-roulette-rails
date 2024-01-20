61831dd0b48fa6518c27ce6068bf6da885811410
Miranda Humphries <mird2794@gmail.com>
2024-01-20 11:18:40 -0500

    check item off shopping list

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,25 @@
 class Api::V1::UsersController < ApplicationController
   def create_shopping_list
-    @user = User.find(params[:user_id])
+    user = User.find(params[:user_id])
 
-    @shopping_list = @user.create_shopping_list!
+    @shopping_list = user.create_shopping_list!
 
     render json: @shopping_list
+  end
+
+  def check_item
+    user = User.find(params[:user_id])
+    item_shopping_list_id = ItemShoppingList.find(item_params[:item_shopping_list_id])
+    item.update!(checked: true)
+
+    render json: { item: item_shopping_list_id, checked: true }
+  end
+
+  def user
+    @user ||= User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:item_shopping_list_id)
   end
 end
